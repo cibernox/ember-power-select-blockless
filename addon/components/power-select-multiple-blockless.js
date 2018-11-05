e86e49d5fd53c2feab44cb5c732469fb94cbc2d6
Lou Greenwood <lougreenwood@me.com>
2018-11-05 10:32:07 +0000

    feat: allow overriding power select component

--- addon/components/power-select-multiple-blockless.js
+++ addon/components/power-select-multiple-blockless.js
@@ -5,6 +5,12 @@ import layout from '../templates/components/power-select-multiple-blockless';
 export default Component.extend({
   layout: layout,
 
+  /**
+   * @property powerSelectMultipleComponent
+   * @type {String}
+   */
+  powerSelectMultipleComponent: 'power-select-multiple',
+
   // CPs
   searchField: computed('labelPath', function() {
     return this.get('labelPath');
